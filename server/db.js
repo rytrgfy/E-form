0d4230e953dfb07bf9050e6dd11ad103dd244d87
Rohit Kumar Sahoo <122625534+rytrgfy@users.noreply.github.com>
2024-06-26 09:57:47 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dburl = 'mongodb+srv://rahul8456:mongodb8456@cluster0.ucbomuy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const dburl = 'add your db link'
 
 module.exports = () => {
     return mongoose.connect(dburl)
